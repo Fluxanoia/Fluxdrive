2b84f2f705fff74802590962fdfd75d4d35f1251
Tyler Wright <6279354+Fluxanoia@users.noreply.github.com>
2020-02-15 00:28:52 +0000

    Added floating point rectangles and points

--- src/maths/fd_maths.cpp
+++ src/maths/fd_maths.cpp
@@ -5,14 +5,34 @@ bool FD_Maths::pointInRect(const int x, const int y, const SDL_Rect* r) {
 	return FD_Maths::pointInRect(&p, r);
 }
 
+bool FD_Maths::pointInRect(const int x, const int y, const FD_Rect* r) {
+	FD_Point p{ x, y };
+	return FD_Maths::pointInRect(&p, r);
+}
+
 bool FD_Maths::pointInRect(const SDL_Point* p, const SDL_Rect* r) {
 	if (p == nullptr || r == nullptr) return false;
 	if (r->w == 0 || r->h == 0) return false;
 	return ((p->x >= r->x) && (p->x <= r->x + r->w)) 
 		&& ((p->y >= r->y) && (p->y <= r->y + r->h));
 }
+bool FD_Maths::pointInRect(const FD_Point* p, const FD_Rect* r) {
+	if (p == nullptr || r == nullptr) return false;
+	if (r->w == 0 || r->h == 0) return false;
+	return ((p->x >= r->x) && (p->x <= r->x + r->w))
+		&& ((p->y >= r->y) && (p->y <= r->y + r->h));
+}
 
 bool FD_Maths::hasIntersection(const SDL_Rect* a, const SDL_Rect* b) {
+	if (a == nullptr || b == nullptr) return false;
+	if (a->w == 0 || a->h == 0 || b->w == 0 || b->h == 0) return false;
+	if (a->x >= b->x + b->w) return false;
+	if (a->x + a->w <= b->x) return false;
+	if (a->y >= b->y + b->h) return false;
+	if (a->y + a->h <= b->y) return false;
+	return true;
+}
+bool FD_Maths::hasIntersection(const FD_Rect* a, const FD_Rect* b) {
 	if (a == nullptr || b == nullptr) return false;
 	if (a->w == 0 || a->h == 0 || b->w == 0 || b->h == 0) return false;
 	if (a->x >= b->x + b->w) return false;
@@ -30,6 +50,21 @@ bool FD_Maths::intersection(const SDL_Rect* a, const SDL_Rect* b, SDL_Rect* inte
 	p.x = (a->x + a->w < b->x + b->w) 
 		? a->x + a->w : b->x + b->w;
 	p.y = (a->y + a->h < b->y + b->h) 
+		? a->y + a->h : b->y + b->h;
+	intersection->w = p.x - intersection->x;
+	intersection->h = p.y - intersection->y;
+	if (intersection->w <= 0) return false;
+	if (intersection->h <= 0) return false;
+	return true;
+}
+bool FD_Maths::intersection(const FD_Rect* a, const FD_Rect* b, FD_Rect* intersection) {
+	if (!hasIntersection(a, b)) return false;
+	SDL_Point p;
+	intersection->x = (a->x > b->x) ? a->x : b->x;
+	intersection->y = (a->y > b->y) ? a->y : b->y;
+	p.x = (a->x + a->w < b->x + b->w)
+		? a->x + a->w : b->x + b->w;
+	p.y = (a->y + a->h < b->y + b->h)
 		? a->y + a->h : b->y + b->h;
 	intersection->w = p.x - intersection->x;
 	intersection->h = p.y - intersection->y;
--- src/maths/fd_maths.hpp
+++ src/maths/fd_maths.hpp
@@ -11,60 +11,91 @@
 //! The defined value of PI used by Fluxdrive.
 #define FD_PI 3.14159
 
+//! The structure defining a Fluxdrive point.
+typedef struct FD_Point_ {
+	//! The x-coordinate of the point.
+	double x;
+	//! The y-coordinate of the point.
+	double y;
+} FD_Point;
+
+//! The structure defining a Fluxdrive rectangle.
+typedef struct FD_Rect_ {
+	//! The x-coordinate of the rectangle.
+	double x;
+	//! The y-coordinate of the rectangle.
+	double y;
+	//! The width of the rectangle.
+	double w;
+	//! The height of the rectangle.
+	double h;
+} FD_Rect;
+
 //! The namespace containing generic mathematical operations.
 namespace FD_Maths {
 
 	//! Checks whether a point is in a rectangle or not.
-	/*!
-		This operation is inclusive of the edges of the rectangle.
-
+	/*! This operation is inclusive of the edges of the rectangle.
 		\param x The x coordinate of the point in question.
 		\param y The y coordinate of the point in question.
 		\param r The rectangle in question.
-
 		\return Returns whether the given point is in the rectangle or not.
-
-		\sa pointInRect
-	*/
+		\sa pointInRect */
 	bool pointInRect(const int x, const int y, const SDL_Rect* r);
 	//! Checks whether a point is in a rectangle or not.
-	/*!
-		This operation is inclusive of the edges of the rectangle.
-
+	/*! This operation is inclusive of the edges of the rectangle.
+		\param x The x coordinate of the point in question.
+		\param y The y coordinate of the point in question.
+		\param r The rectangle in question.
+		\return Returns whether the given point is in the rectangle or not.
+		\sa pointInRect */
+	bool pointInRect(const int x, const int y, const FD_Rect* r);
+	//! Checks whether a point is in a rectangle or not.
+	/*! This operation is inclusive of the edges of the rectangle.
 		\param p The point in question.
 		\param r The rectangle in question.
-
 		\return Returns whether the given point is in the rectangle or not.
-
-		\sa pointInRect
-	*/
+		\sa pointInRect */
 	bool pointInRect(const SDL_Point* p, const SDL_Rect* r);
+	//! Checks whether a point is in a rectangle or not.
+	/*! This operation is inclusive of the edges of the rectangle.
+		\param p The point in question.
+		\param r The rectangle in question.
+		\return Returns whether the given point is in the rectangle or not.
+		\sa pointInRect */
+	bool pointInRect(const FD_Point* p, const FD_Rect* r);
 	//! Checks whether two rectangles intersect.
-	/*!
-		This operation is inclusive of the edges of the rectangles.
-
+	/*! This operation is inclusive of the edges of the rectangles.
 		\param a The first rectangle in question.
 		\param b The second rectangle in question.
-
 		\return Returns whether the rectangle intersect.
-
-		\sa intersection
-	*/
+		\sa intersection */
 	bool hasIntersection(const SDL_Rect* a, const SDL_Rect* b);
+	//! Checks whether two rectangles intersect.
+	/*! This operation is inclusive of the edges of the rectangles.
+		\param a The first rectangle in question.
+		\param b The second rectangle in question.
+		\return Returns whether the rectangle intersect.
+		\sa intersection */
+	bool hasIntersection(const FD_Rect* a, const FD_Rect* b);
 	//! Calculates the intersection of two rectangles.
-	/*!
-		This operation is inclusive of the edges of the rectangles.
+	/*! This operation is inclusive of the edges of the rectangles.
 		The intersection is written to the intersection parameter.
-
 		\param a            The first rectangle in question.
 		\param b            The second rectangle in question.
 		\param intersection The rectangle for the intersection to be written to.
-
 		\return Returns whether the rectangle intersect.
-
-		\sa hasIntersection
-	*/
+		\sa hasIntersection */
 	bool intersection(const SDL_Rect* a, const SDL_Rect* b, SDL_Rect* intersection);
+	//! Calculates the intersection of two rectangles.
+	/*! This operation is inclusive of the edges of the rectangles.
+		The intersection is written to the intersection parameter.
+		\param a            The first rectangle in question.
+		\param b            The second rectangle in question.
+		\param intersection The rectangle for the intersection to be written to.
+		\return Returns whether the rectangle intersect.
+		\sa hasIntersection */
+	bool intersection(const FD_Rect* a, const FD_Rect* b, FD_Rect* intersection);
 	
 }
 
